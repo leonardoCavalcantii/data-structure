3c539da25bf32155e3006d0e9b6ebd1e6827c278
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2024-04-11 20:01:06 -0300

    Update PotenciaRecursiva.java

--- PotenciaRecursiva.java
+++ PotenciaRecursiva.java
@@ -5,27 +5,17 @@ class PotenciaRecursiva {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         int i = sc.nextInt();
         int j = sc.nextInt();
         System.out.println(recursive(i, j));
-
     }
-
     public static int recursive(int i, int j){
-        
-        if(j == 0){
+        if(j == 0)
             return 1;
-        }
-
-        if(j == 1){
+        if(j == 1)
             return i;
-        }
-
         return i * recursive(i, -- j);
-
         }
-
     }
     
 
