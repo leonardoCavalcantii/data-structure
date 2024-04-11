a0c94fa582d1107c91e2717f6180364defa7c29a
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2024-04-11 20:09:27 -0300

    Update TrocaVizinhoLL.java

--- TrocaVizinhoLL.java
+++ TrocaVizinhoLL.java
@@ -11,101 +11,66 @@ import java.util.Scanner;
 
          for (int i = 0; i < seq.length; i++) {
             lista.addLast(Integer.parseInt(seq[i]));
-            }
-            
+            }  
             lista.swap(x);
-            
-            System.out.println(lista.toString().trim());
-         
-
-    
+            System.out.println(lista.toString().trim()); 
     }
-
 }
 class LeoList{
-        
-        Node head;
-        Node tail;
-
-        public void addLast(int x){
-            Node node = new Node(x);
-            
+    Node head;
+    Node tail;
+    public void addLast(int x){
+        Node node = new Node(x);  
             if(head == null){
                 head = node;
-                tail = node;
-            
-            }else{
-                
+                tail = node; 
+            }else{  
                 tail.next = node;
                 node.pre = tail;
                 tail = node;
-
             }
-
         }
-
         public void swap(int i){
-
             Node auxA = head;
-            
             for (int j = 0; j < i; j++) {
              auxA = auxA.next;   
             }
-            
             Node auxB = auxA.next;
-            
             auxA.next = auxB.next;
             auxB.pre = auxA.pre;
             
             if(auxB.next!= null){
                 auxB.next.pre = auxA;    
-                
             }else{
                 tail = auxA;
             }
-            
             auxA.pre = auxB;
             auxB.next = auxA;
             
             if(auxB.pre != null){
                 auxB.pre.next = auxB;
-            
             }else{
                 head = auxB;
-            
             }
-
         }
         @Override
         public String toString(){
-            
             String lista = "";
             Node aux = head;
             
             while(aux != null){
                 lista += aux.value + " ";
                 aux = aux.next;
-
             }
-            
             return lista;
-
-        }
-
-    
-    
+        }    
     }
-
-
     class Node{
-        
         int value;
         Node pre;
         Node next;
-
+        
         public Node(int x){
             value = x;
         }
-
-    
     }
