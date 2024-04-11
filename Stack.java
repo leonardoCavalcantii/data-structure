7b37494cba813a45c5a15df128557254711510bf
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2024-04-11 20:06:28 -0300

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,54 +1,43 @@
-//revisão stack(pilha)
 class Stack{
 
-private T[] array;
+ private T[] array;
  private int top;
-
 
 public T top(){
     this.top = -1;
-
 }
     
 public boolean isEmpity(){
-    
     if(top == -1){
         return true;
-    
     }else {
         return false;
     }
 }
 
 public boolean isFull(){
-    
     if(top == array.length -1){
         return true;
-    
     } else{
         return false;
     }
 }
 
-public pvoid push(T index){
-    
+public pvoid push(T index){    
     if(isFull()){
         throw new StackOverFlowException();
-
     } else{
         index ++; this.array[top] = index;
     }
 }
 
 public T pop(){
-    
     if(isEmpity()){
         throw new StackUnderFlowExcepition();
-    
     } else{
         T elemnt = this.array[top];
         top--;
         return element;
     }
-}
+  }
 }
