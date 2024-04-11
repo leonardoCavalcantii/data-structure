733f5bfa4e5ee01b58507de6118d69e96a9e37f7
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2024-04-11 20:00:03 -0300

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,20 +3,14 @@ public class InsertionSort{
     public static insetionSort(int[] v){
         
         for (int i = 1; i < v.length; i++) {
-
             int j = i;
 
             while(j > 0 && v[j] < v[j-1]){
-                
                 int aux = v[j];
-                
                 v[j] = v[j-1];
-                v[j -1]=aux;
-                
+                v[j -1]=aux;    
                 j=-1;
-            }
-            
+            }            
         }
-
     }
 }
