2b20b72d4cde64d55b850a5a78e5bdc8749274c7
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2024-10-18 20:15:29 -0300

    Update BuscaBinaria.java

--- BuscaBinaria.java
+++ BuscaBinaria.java
@@ -1,28 +1,20 @@
 class BuscaBinaria{
-
-    public boolean search(int x, int v[], int leftIndex, int rightIndex){
-
-        int mid = (leftIndex + rightIndex )/ 2;
-
-        //caso o x não pertença aos limites do array.
-        if(leftIndex > rightIndex){
-            return false;
+    public int search(int x, int v[], int leftIndex, int rightIndex){
         
+        if(leftIndex > rightIndex){
+            return -1;        
         }
-        //verifica se o x se encontra no meio, se sim, retorna "mid".
+    
+        int mid = (leftIndex + rightIndex ) / 2;
+        
         if(x == v[mid]){
             return mid;
         }
-        //verfifica se o x se encontra na primeira metade do array, por isso "mid - 1".
         if(x < v[mid]){
-            return search(x, v, leftIndex, mid - 1);
-        
+            return search(x, v, leftIndex, mid - 1); 
         }
-        //verfifica se o x se encontra na segunda metade do array, por isso "mid + 1".
         else{
             return search(x, v, mid + 1, rightIndex);
-        }
-
-        
+        }     
     }
 }
