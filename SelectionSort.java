01374c48edb48bfca71621b495655769d130079e
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2024-10-18 20:09:24 -0300

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class SelectionSort {
     public static void selectionSort(int[] vetor) {
         for (int i = 0; i < vetor.length; i++) {
@@ -12,17 +10,6 @@ public class SelectionSort {
                 vetor[i] = vetor[i_menor];
                 vetor[i_menor] = aux;
             }
-
         }
-
     }
-
-    public static void main(String[] args) {
-        int[] vetor = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-        int[] ordenado = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        selectionSort(vetor);
-        assert Arrays.equals(vetor, ordenado);
-
-    }
-
 }
