f7b5ba305429c007ffaa328bf85651f8f2bf4b5e
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2024-10-18 20:10:39 -0300

    Update SumRec.java

--- SumRec.java
+++ SumRec.java
@@ -3,13 +3,5 @@ class SumRec {
         if (index > vetor.length - 1)
             return 0;
         return vetor[index] + sumRec(vetor, index + 1);
-
     }
-
-    public static void main(String[] args) {
-        int[] vetor = { 1, 2, 3, 4, 5 };
-        assert sumRec(vetor, 0) == 15;
-
-    }
-
 }
